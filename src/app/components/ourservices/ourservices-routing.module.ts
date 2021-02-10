c032225464fa9936162559e53bc3fa277ea186f9
mandarchandak <mandar.chandak@aquilmedia.in>
2021-02-10 20:59:52 +0530

    new pages

---
+++ src/app/components/ourservices/ourservices-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OurservicesComponent } from './ourservices.component';
+
+const routes: Routes = [{ path: '', component: OurservicesComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OurservicesRoutingModule { }
