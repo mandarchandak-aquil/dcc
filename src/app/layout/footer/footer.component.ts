5d8d9b8380c3aa5d77c0623b0d89cd67bbd9062f
pravindatrange <pravindatrange@gmail.com>
2021-02-24 20:48:00 +0530

    changes

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -19,7 +19,7 @@ export class FooterComponent implements OnInit {
   footerlogocontent:any;
   submitted = false;
   success:boolean = false;
-
+  failed: boolean = false;
   constructor(public comman:CommonService,private route: ActivatedRoute,private sanitizer: DomSanitizer,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -60,29 +60,39 @@ export class FooterComponent implements OnInit {
 
   hidemsg(){
     setTimeout(() => {
-      this.email.controls.email.clearValidators()
+      // this.email.controls.email.clearValidators()
       this.email.controls.email.setErrors(null)
-      this.email.controls.email.markAsPristine()
-    }, 5000);
+      // this.email.controls.email.markAsPristine()
+    }, 3000);
   }
 
   // get name() { return this.email.get('email'); }
 
   onSubmit(){
     this.submitted = true;
-    if (this.email.invalid) {
-      return;
+    if (this.email.controls.email.value == '') {
+      this.failed = true;
+      setTimeout(() => {
+        this.failed = false;
+      }, 3000);
+    }else{
+      this.comman.subscribeform(this.email.value).subscribe(data => {
+        if(data['response'] == 200){
+          this.email.reset();
+          // this.email.controls.email.setValue('')
+          this.email.controls.email.setValidators(Validators.required);
+          this.email.controls.email.setValidators(Validators.email);
+          this.email.updateValueAndValidity(); 
+          // this.email.reset();
+          this.success = true;
+          setTimeout(() => {
+            this.success = false;
+          }, 3000);
+  
+        }
+     console.log(data,'send');
+    });
     }
-    this.comman.subscribeform(this.email.value).subscribe(data => {
-      if(data['response'] == 200){
-        this.email.reset();
-        this.success = true;
-        setTimeout(() => {
-          this.success = false;
-        }, 3000);
-
-      }
-   console.log(data,'send');
-  });
+    
   }
 }
